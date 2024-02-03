90e4bb3ad4ea3af9a31ffc483e8fccc731aad160
Ana Lopes <alopes.work@protonmail.com>
2024-02-03 21:41:56 +0000

    Account Fragment

--- app/src/main/java/com/pac/imonline/activity/Constant.java
+++ app/src/main/java/com/pac/imonline/activity/Constant.java
@@ -5,6 +5,7 @@ public class Constant {
     public static final String URL = "http://10.0.2.2:8000/";
     public static final String HOME = URL+"ApiImOnline";
     public static final String LOGIN = HOME+"/login";
+    public static final String LOGOUT = HOME+"/logout";
     public static final String REGISTER = HOME+"/register";
     public static final String SAVE_USER_INFO = HOME+"/save_user_info";
     public static final String POSTS = HOME+"posts";
@@ -15,5 +16,6 @@ public class Constant {
     public static final String COMMENTS = POSTS+"/comments";
     public static final String CREATE_COMMENT = HOME+"/comments/create";
     public static final String DELETE_COMMENT = HOME+"/comments/delete";
+    public static final String MY_POST = POSTS+"/my_posts"
 
 }
